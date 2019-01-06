e23fd614a453e841a4a306fd05feb75407253197
Hiroki <daivd.scheiklies@gmail.com>
2019-01-06 05:51:01 +0100

    added a check to Threaded_master so the programm doeen't stop if threads are still working

--- utility_video/thread_master.py
+++ utility_video/thread_master.py
@@ -1,18 +1,18 @@
 from threded_image_processing import processing_thread
 from os import listdir
 import json
+import time
 
 
 def get_config():
     file = open("config.json", "r")
-    json_string = file.read(90000)
-    
+    json_string = file.read(90000)   
     return json.loads(json_string)
+
 
 def get_image_count(path):
     all_images = listdir(path)
     return len(all_images)
-
 
 
 class thread_orcastrator():
@@ -39,7 +39,14 @@ class thread_orcastrator():
             index = index + 1
         print(inactivThreads)    
         return inactivThreads
-            
+
+    def check_is_thead_still_working(self):
+        for thread in self.threads:
+            if thread.isAlive() is True:
+                return True
+            else:
+                return False
+
     def run_thread(self, n, image_range):
         self.threads[n].run(image_range)
 
@@ -51,13 +58,19 @@ class thread_orcastrator():
     def start(self):
         self.create_threads()
 
-        while True : #self.current_count < self.image_count
+        while True : # self.current_count < self.image_count
             
             inactivThreads = self.check_thead_working()
             
             for n in inactivThreads:
 
                 self.run_thread(n, self.get_next_frame_range())
+        
+        while True:
+            if self.check_is_thead_still_working() is False:
+                break
+            else:
+                time.sleep(1)                
 
 
 config = get_config()
